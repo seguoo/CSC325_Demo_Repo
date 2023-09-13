cd9ac742ac9747443600ca1c70e0a2d2152f8b19
seguoo <seguo@farmingdale.edu>
2023-09-12 23:04:30 -0400

    initial commit

---
+++ src/main/java/com/mycompany/csc325_demo01/CSC325_Demo01.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.csc325_demo01;
+
+/**
+ *
+ * @author omarsegura
+ */
+public class CSC325_Demo01 {
+
+    public static void main(String[] args) {
+        System.out.println("Omar Segura \n\n\n");
+        System.out.println("Omar Segura \n\n\n");
+        
+    }
+}
